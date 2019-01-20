45cb8da8d459db24be3692ddce4ca8bafa1d17fe
Rudolf Hartmann <rudolf.hartmann@prime-force.com>
2019-01-20 20:59:38 +0100

    added edit save button

--- client/src/Content.js
+++ client/src/Content.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import ReactDOM from "react-dom";
-
+// imports CSS 
 import './Content.css';
 
 const iframeStyle = {
@@ -8,11 +7,33 @@ const iframeStyle = {
     height: '500px'
 };
 
+const buttonStyle = {
+    /* Edit savebutton right */
+  };
 
 export class Content extends Component {
     constructor(props) {
         super(props);
-        this.myRef = React.createRef();
+        this.state = {isDesignModeOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+    }
+
+    componentDidMount(){
+        console.log("iife");
+    }
+
+    handleClick() {
+        const frame = document.getElementsByName ("richtTextField")[0];
+        if(this.state.isDesignModeOn) {
+            frame.contentWindow.document.designMode = 'On';
+        } else {
+            frame.contentWindow.document.designMode = 'Off';
+        }
+        this.setState(prevState => ({
+            isDesignModeOn: !prevState.isDesignModeOn
+        }));
     }
 
     render() {
@@ -20,10 +41,11 @@ export class Content extends Component {
             <div className="main">
                 <button title="bold"><i className="fa fa-bold"></i></button>
                 <button title="italic"><i className="fa fa-italic"></i></button>
+                <button onClick={this.handleClick} styple={buttonStyle}>
+                    {this.state.isDesignModeOn ? 'Edit' : 'Save'}
+                </button>
                 <br />
-                <iframe name="richtTextField" style={iframeStyle} />
-                <div ref={this.myRef} />
-
+                <iframe name="richtTextField"  style={iframeStyle}/>
             </div>
         );
     }
