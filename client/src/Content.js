c273a7e08b2d30154646d9592b986d662c58f065
Rudolf Hartmann <rudolf.hartmann@prime-force.com>
2019-01-22 18:54:47 +0100

    next iteration

--- client/src/Content.js
+++ client/src/Content.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 // imports CSS 
 import './Content.css';
 
@@ -7,29 +8,39 @@ const iframeStyle = {
     height: '500px'
 };
 
-const buttonStyle = {
-    /* Edit savebutton right */
-  };
-
 export class Content extends Component {
     constructor(props) {
         super(props);
-        this.state = {isDesignModeOn: true};
+        this.state = {
+            isDesignModeOn: true,
+            richTextContent: "Hallo hier ist ganz viel Text"
+        };
 
     // This binding is necessary to make `this` work in the callback
     this.handleClick = this.handleClick.bind(this);
     }
 
     componentDidMount(){
-        console.log("iife");
+        this.ifr.contentWindow.document.body.innerHTML = this.state.richTextContent;
     }
 
+    updateRichTextContent() {
+        let updateText = this.ifr.contentWindow.document.body.innerHTML;
+        this.setState((state) => {
+          return {richTextContent: updateText}
+        });
+        console.log("In updateRichTextContent", this.state.richTextContent);
+      }
+      
     handleClick() {
-        const frame = document.getElementsByName ("richtTextField")[0];
+        
+        
+        const frame = document.getElementsByName ("richTextField")[0];
         if(this.state.isDesignModeOn) {
             frame.contentWindow.document.designMode = 'On';
         } else {
             frame.contentWindow.document.designMode = 'Off';
+            this.updateRichTextContent();
         }
         this.setState(prevState => ({
             isDesignModeOn: !prevState.isDesignModeOn
@@ -41,11 +52,11 @@ export class Content extends Component {
             <div className="main">
                 <button title="bold"><i className="fa fa-bold"></i></button>
                 <button title="italic"><i className="fa fa-italic"></i></button>
-                <button onClick={this.handleClick} styple={buttonStyle}>
+                <button onClick={this.handleClick}>
                     {this.state.isDesignModeOn ? 'Edit' : 'Save'}
                 </button>
                 <br />
-                <iframe name="richtTextField"  style={iframeStyle}/>
+                <iframe name="richTextField"  style={iframeStyle} ref={(r) => this.ifr = r} />
             </div>
         );
     }
